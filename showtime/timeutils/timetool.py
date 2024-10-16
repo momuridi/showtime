5a16341d118befad261ba29df66aa8731a24913a
Mohamed Muridi <mohamed.muridi@ruhlgroup.com>
2024-10-16 16:01:11 +0200

    Fixed local time

--- showtime/timeutils/timetool.py
+++ showtime/timeutils/timetool.py
@@ -2,15 +2,17 @@ from datetime import datetime
 import pytz
 from os import getenv
 
-def get_time(timezone) -> str:
+# Get current time in specified timezone
+def format_timenow(tz) -> str:
     time_format = "%d.%m.%Y %H:%M"
-    return datetime.now(timezone).strftime(time_format)
+    return datetime.now(tz).strftime(time_format)
 
+# Get current time in environment timezone
 def environment_time_now():
-    TIMECONF = pytz.timezone(getenv('TIME_ENV', local_timezone()))
-    return get_time(TIMECONF)
-
-def local_timezone() -> str:
-    utc_now = datetime.now(pytz.utc)
-    local_now = utc_now.astimezone()
-    return local_now.tzname()
+    tz = getenv('TIME_ENV')
+    if tz:
+        TIMECONF = pytz.timezone(tz)
+    else:
+        TIMECONF = None
+    return format_timenow(TIMECONF)
+    
